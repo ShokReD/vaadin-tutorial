0c0bad14436da3c17f7e58983e1d1fd9ab3709d1
shokred <shokred@yandex.ru>
2019-08-28 21:52:45 +0700

    Add filter text field

--- src/main/kotlin/name/shokred/demo/vaadin/view/MainView.kt
+++ src/main/kotlin/name/shokred/demo/vaadin/view/MainView.kt
@@ -2,6 +2,8 @@ package name.shokred.demo.vaadin.view
 
 import com.vaadin.flow.component.grid.Grid
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.data.value.ValueChangeMode
 import com.vaadin.flow.router.Route
 import name.shokred.demo.vaadin.domain.Customer
 import name.shokred.demo.vaadin.service.CustomerService
@@ -10,16 +12,26 @@ import name.shokred.demo.vaadin.service.CustomerService
 class MainView() : VerticalLayout() {
     private val service = CustomerService.INSTANCE
     private val grid = Grid(Customer::class.java)
+    private val filterText = TextField()
 
     init {
+        filterText.placeholder = "Filter by name..."
+        filterText.isClearButtonVisible = true
+        filterText.valueChangeMode = ValueChangeMode.EAGER
+        filterText.addValueChangeListener {
+            updateList()
+        }
+
         grid.setColumns("firstName", "lastName", "status")
-        add(grid)
+
+        add(filterText, grid)
+
         setSizeFull()
 
         updateList()
     }
 
     private fun updateList() {
-        grid.setItems(service.findAll())
+        grid.setItems(service.findAll(filterText.value))
     }
 }
